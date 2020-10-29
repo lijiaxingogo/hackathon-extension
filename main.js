ef70892155c78d476d5ecc9f6c8f079e8378de42
Tobi-Wan Rodriguez <61915985+rtobiwan@users.noreply.github.com>
2020-10-28 23:59:09 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const contents = document.getElementById('hplogo');
+const newText = document.createElement('p')
+newText.textContent = 'This is a test'
+contents.parentNode.appendChild(newText)
+//contents.parentNode.removeChild(contents);
+// let profaneWords = ['porn', 'anal', 'fuck', 'sex', 'shit', 'ass', 'fucker', 'pussy', 'dick', 'motherfucker'];
